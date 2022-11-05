c793925ad1d285fdf63841e8aca34cb8d31a9faf
Rodney Gauna <rodneygauna@gmail.com>
2022-11-04 18:05:49 -0700

    fixed a typo

--- draw.lua
+++ draw.lua
@@ -1,6 +1,10 @@
 -- draw.lua
 
 function draw_menu()
-    cls(1)
+    cls(0)
     print("20 second potions",34,40,12)
+    print("how to play",40,60,10)
+    print("instructions go here",46,68, 10)
+    print("made by s0n0f4l1ch",57,110,13)
+    print("20 second game jam",57,120,13)
 end
