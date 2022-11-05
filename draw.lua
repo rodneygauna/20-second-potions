a85ca3ff969bb6d066ad96e7961b5eef494f2400
Rodney Gauna <rodneygauna@gmail.com>
2022-11-05 16:58:12 -0700

    getting the bg for the menu to work

--- draw.lua
+++ draw.lua
@@ -2,9 +2,11 @@
 
 function draw_menu()
     cls(0)
+    bgpot()
     print("20 second potions",34,40,12)
     print("how to play",40,60,10)
     print("instructions go here",46,68, 10)
     print("made by s0n0f4l1ch",57,110,13)
     print("20 second game jam",57,120,13)
+
 end
--- tools.lua
+++ tools.lua
@@ -1,1 +1,26 @@
 -- tools.lua
+
+function bgpot()
+    pots={}
+    -- generates pots and
+    -- randomizes their x,y, and spd
+    for i=1,35 do
+        local newpot={}
+        newpot.spr=flr(rnd(52))
+        newpot.x=flr(rnd(128))
+        newpot.y=flr(rnd(128))
+        newpot.spd=rnd(1)
+        add(pots,newpot)
+    end
+    for i=1,#pots do
+        local bgpots=pots[i]
+        spr(bgpots.spr, bgpots.x,bgpots.y)
+    end
+    for i=1,#pots do
+        local bgpots=pots[i]
+        bgpots.y=bgpots.y+bgpots.spd
+        if bgpots.y>128 then
+            bgpots.y=bgpots.y-128
+        end
+    end
+end
